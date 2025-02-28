628a09e42ee0663280171acdd23869b536d6b895
paco7828 <koszegipatrik888@gmail.com>
2025-02-28 20:04:57 +0100

    Added basic GPS code.

---
+++ v_da-detector/GPS/GPS.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+#define GPS_RX 2
+#define GPS_TX 6
+#define GPS_BAUD 9600
+
+SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
+TinyGPSPlus gps;
+
+double currentLat, currentLon;
+int currentSpeed;
+
+void setup() {
+  Serial.begin(115200);
+  gpsSerial.begin(GPS_BAUD);
+}
+
+void loop() {
+  while (gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+    if (gps.location.isValid()) {
+        // Valid GPS data
+        currentLat = gps.location.lat();
+        currentLon = gps.location.lng();
+        currentSpeed = gps.speed.kmph();
+    } else {
+      // Waiting for GPS data
+      Serial.println("No GPS fix yet...");
+    }
+  }
+}
